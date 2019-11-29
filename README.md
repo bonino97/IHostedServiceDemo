bc58bc9c5ef29ff8263262c9fc28bb2e23b0b5d2
bonino97 <juanbonino97@gmail.com>
2019-11-28 21:42:50 -0300

    Comit Inicial
    Comit Inicial

---
+++ README.md
@@ -0,0 +1,2 @@
+# IHostedServiceDemo
+ Ejecucion de Tareas Recurrentes, Procesos y escritura de un archivo de texto en tiempo real.
---
+++ Services/WriteToFileHostedService.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IHostedServiceDemo.Services
+{
+    public class WriteToFileHostedService : IHostedService, IDisposable
+    {
+        private readonly IHostingEnvironment environment;
+        private readonly string fileName = "Archivo-1.txt";
+        private Timer timer;
+
+        public WriteToFileHostedService(IHostingEnvironment environment)
+        {
+            this.environment = environment;
+        }
+
+        public void Dispose()
+        {
+            timer?.Dispose();
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            WriteToFile("WriteToFileHostedService: Process Started.");
+            timer = new Timer(DoWork, null, TimeSpan.Zero, TimeSpan.FromSeconds(5));
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            WriteToFile("WriteToFileHostedService: Process Finished.");
+            timer?.Change(Timeout.Infinite, 0);
+            return Task.CompletedTask;
+        }
+        private void DoWork(object state)
+        {
+            WriteToFile("WriteToFileHostedService: Doing some work at " + DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss"));
+        }
+        private void WriteToFile(string message)
+        {
+            var path = $@"{environment.ContentRootPath}\wwwroot\{fileName}";
+            using (StreamWriter writer = new StreamWriter(path, append: true))
+            {
+                writer.WriteLine(message);
+            }
+        }
+    }
+}
